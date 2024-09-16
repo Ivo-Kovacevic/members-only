8812250399d00c76ee3cfc3286625314cd72e983
Ivo <131997439+Ivo-Kovacevic@users.noreply.github.com>
2024-09-16 11:43:11 +0200

    Add input field for confirming password while registering

--- validation/user-validation.js
+++ validation/user-validation.js
@@ -1,8 +1,9 @@
-const { check } = require("express-validator");
+const { check, body } = require("express-validator");
 
 const alphaErr = "must only contain letters and numbers.";
 const usernameLengthErr = "must be between 4 and 20 characters.";
 const passwordLengthErr = "must be at least 4 characters long.";
+const passwordMatchErr = "Passwords do not match";
 
 const validateNewUser = [
     check("username")
@@ -16,6 +17,15 @@ const validateNewUser = [
         .trim()
         .isLength({ min: 4 })
         .withMessage(`Password ${passwordLengthErr}`),
+
+    body("passwordConfirm")
+        .trim()
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error(passwordMatchErr);
+            }
+            return true;
+        }),
 ];
 
 module.exports = { validateNewUser };
